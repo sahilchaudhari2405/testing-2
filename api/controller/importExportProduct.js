57e4f72eb87d3ce4218aa616dd7d11ef1c8faf22
shankjbs571 <shankjbs571@gmail.com>
2024-08-01 15:03:05 +0530

    Refactor Admin Dashboard and Charts

--- api/controller/importExportProduct.js
+++ api/controller/importExportProduct.js
@@ -2,48 +2,49 @@ import Product from '../model/product.model.js';
 
 export const importProducts = async (req, res) => {
   const products = req.body.products;
+  console.log("inside importprodcut controller received product are :",products);
 
-  try {
-    const importedProducts = [];
-    const skippedProducts = [];
+  // try {
+  //   const importedProducts = [];
+  //   const skippedProducts = [];
 
-    for (const productData of products) {
-      // Remove the _id field if it exists to avoid duplicate key error
-      delete productData._id;
+  //   for (const productData of products) {
+  //     // Remove the _id field if it exists to avoid duplicate key error
+  //     delete productData._id;
 
-      let existingProduct = await Product.findOne({ slug: productData.slug });
+  //     let existingProduct = await Product.findOne({ slug: productData.slug });
 
-      if (existingProduct) {
-        // Skip the product if it already exists based on the slug
-        skippedProducts.push(productData);
-        continue;
-      }
+  //     if (existingProduct) {
+  //       // Skip the product if it already exists based on the slug
+  //       skippedProducts.push(productData);
+  //       continue;
+  //     }
 
-      existingProduct = await Product.findOne({ BarCode: productData.BarCode });
+  //     existingProduct = await Product.findOne({ BarCode: productData.BarCode });
 
-      if (existingProduct) {
-        // Find the maximum integer barcode value in the database
-        const maxBarcodeProduct = await Product.findOne().sort({ BarCode: -1 }).limit(1);
-        let newBarcode = maxBarcodeProduct ? maxBarcodeProduct.BarCode + 1 : 1;
+  //     if (existingProduct) {
+  //       // Find the maximum integer barcode value in the database
+  //       const maxBarcodeProduct = await Product.findOne().sort({ BarCode: -1 }).limit(1);
+  //       let newBarcode = maxBarcodeProduct ? maxBarcodeProduct.BarCode + 1 : 1;
         
-        productData.BarCode = newBarcode;
-      }
+  //       productData.BarCode = newBarcode;
+  //     }
 
-      const product = new Product(productData);
-      const savedProduct = await product.save();
-      importedProducts.push(savedProduct);
-    }
+  //     const product = new Product(productData);
+  //     const savedProduct = await product.save();
+  //     importedProducts.push(savedProduct);
+  //   }
 
-    res.json({
-      message: "Products imported successfully",
-      status: true,
-      data: importedProducts,
-      skipped: skippedProducts,
-    });
-  } catch (error) {
-    console.error('Error processing products:', error);
-    res.status(500).json({ success: false, error: 'Internal Server Error' });
-  }
+  //   res.json({
+  //     message: "Products imported successfully",
+  //     status: true,
+  //     data: importedProducts,
+  //     skipped: skippedProducts,
+  //   });
+  // } catch (error) {
+  //   console.error('Error processing products:', error);
+  //   res.status(500).json({ success: false, error: 'Internal Server Error' });
+  // }
 };
 
 
