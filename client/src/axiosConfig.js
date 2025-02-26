2800310ed6ef6fa4f7167ae17402babf5ad92a36
sahil chaudhari <sahilchaudhari@Mac.lan>
2025-02-26 13:56:00 +0530

    Add the axios

--- client/src/axiosConfig.js
+++ client/src/axiosConfig.js
@@ -12,7 +12,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: process.env.REACT_APP_API_URL || 'http://localhost:4000',// Change this to your API base URL
+  baseURL:'https://software.apalabajar.shop/api',// Change this to your API base URL
   withCredentials: true, // Include credentials in requests 
   //process.env.REACT_APP_BASE_URL ||
 }); 
